fd6e241f5dbb1ca4d82817531d4a428ed2ce7823
yunanhelmi <yunanhelmimahendra@gmail.com>
2015-07-09 13:47:19 +0700

    Setting config, autoload, database

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@
 | environments.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = '/pbb/';
 
 /*
 |--------------------------------------------------------------------------
@@ -226,7 +226,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'madiun';
 
 /*
 |--------------------------------------------------------------------------
